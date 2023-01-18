f799893363b1d73260287db5ef470c8b3ec42d0d
ninjabro <arunzaaron@gmail.com>
2023-01-18 22:49:06 +0530

    stats component

---
+++ src/components/Stats.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "./stats.module.scss";
+import groupimage from "../images/image-header-desktop.jpg";
+import mobile from "../images/image-header-mobile.jpg";
+
+function Stats() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left__content}>
+        <h1 className={styles.title}>
+          Get <span className={styles.insights}> insights </span> that help your
+          business grow.
+        </h1>
+
+        <div className={styles.description}>
+          {" "}
+          <p>
+            {" "}
+            Discover the benefits of data analytics and make better decisions
+            regarding revenue, customer experience, and overall efficiency.
+          </p>
+        </div>
+        <div className={styles.stats}>
+          <div className={styles.stats__content}>
+            {" "}
+            <h1>10k+</h1>
+            <p>companies</p>{" "}
+          </div>
+          <div className={styles.stats__content}>
+            <h1>314</h1> <p>templates</p>{" "}
+          </div>
+          <div className={styles.stats__content}>
+            {" "}
+            <h1>12m+</h1> <p>queries</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.right__content}>
+        <picture>
+          <source media="(min-width:375px )" srcset={mobile} />
+          <img src={groupimage} alt="img" className="img" />
+        </picture>
+      </div>
+    </div>
+  );
+}
+
+export default Stats;
